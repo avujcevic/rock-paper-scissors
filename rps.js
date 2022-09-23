c7e94fb3920ea874e88498b6c6ad1eeaa5d17748
andrew <a.vujcevic@gmail.com>
2022-09-22 21:02:59 -0600

    Play one round of game with "Rock" as player input

--- rps.js
+++ rps.js
@@ -7,8 +7,27 @@ function getComputerChoice() {
     } else if (ranNum === 1) {
         choice = "Paper";
     } else {
-        choice = "Scissors"
+        choice = "Scissors";
     }
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    let winningMessage;
+
+    if (playerSelection === computerSelection) {
+        winningMessage = `Tie! ${playerSelection} ties ${computerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        winningMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        winningMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    return winningMessage;
+}
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
